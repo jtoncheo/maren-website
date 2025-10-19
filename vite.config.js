4c503ea3e06dc0745f9c2386b0e93b451caffaea
Cmandapat <cmandapat1215@gmail.com>
2025-10-19 17:11:25 -0500

    to deploy

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: "/maren-website/",
   plugins: [react()],
   resolve: {
     alias: {
