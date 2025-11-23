019a6d2a51e69c905d88af289995becb8630c4e1
jtoncheo <jesustoncheochoa@gmail.com>
2025-11-23 16:43:38 -0600

    Hamburger test

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -32,7 +32,7 @@ function Header() {
         <Link to="/about">About</Link>
         <Link to="/available-homes">Available Homes</Link>
         <Link to="/past-projects">Past Projects</Link>
-        <Link to="/contact">Contact</Link>
+        <a ref="/contact">Contact</a>
       </nav>
     </header>
   );
