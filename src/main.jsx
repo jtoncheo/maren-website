cd386cd8130fcbdfb8b77d00ad8d087fa5fff91e
jtoncheo <jesustoncheochoa@gmail.com>
2026-01-01 19:25:08 -0600

    Fixing routing

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App.jsx';
 import './index.css';
 
-const basename = import.meta.env.MODE === 'production' ? '/maren-website' : '/';
+const basename = '/'; 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
